5a1d46f4eb2e70ef861552cc36a313833b0c14b8
C@Z <cz.theng@gmail.com>
2016-06-02 15:25:57 +0800

    mpdemo

---
+++ examples/mpdemo/mpdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mpdemo
+//
+//  Created by apollo on 16/6/2.
+//  Copyright © 2016年 projm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
