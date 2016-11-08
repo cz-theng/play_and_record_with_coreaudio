7eeddd37bed825f44084fcaf1461373894d288be
CZ <cz.theng@gmail.com>
2016-11-08 10:48:38 +0800

    add avaudiorecorderdemo

---
+++ avfoundation/examples/AVAudioRecoderDemo/AVAudioRecoderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAudioRecoderDemo
+//
+//  Created by apollo on 08/11/2016.
+//  Copyright © 2016 projm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ avfoundation/examples/AVAudioRecoderDemo/AVAudioRecoderDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVAudioRecoderDemo
+//
+//  Created by apollo on 08/11/2016.
+//  Copyright © 2016 projm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
