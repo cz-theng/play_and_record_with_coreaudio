d0c2ad4adcdc9a6806ed9c1230c879a2899cf8a6
CZ <cz.theng@gmail.com>
2016-12-14 19:24:27 +0800

    add AudioQueueReader Demo

---
+++ audiotoolbox/audioqueue/examples/AudioQueueRecorderDemo/AudioQueueRecorderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioQueueRecorderDemo
+//
+//  Created by apollo on 14/12/2016.
+//  Copyright © 2016 projm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ audiotoolbox/audioqueue/examples/AudioQueueRecorderDemo/AudioQueueRecorderDemoTests/AudioQueueRecorderDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  AudioQueueRecorderDemoTests.m
+//  AudioQueueRecorderDemoTests
+//
+//  Created by apollo on 14/12/2016.
+//  Copyright © 2016 projm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioQueueRecorderDemoTests : XCTestCase
+
+@end
+
+@implementation AudioQueueRecorderDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
